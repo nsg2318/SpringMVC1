d75fd9497ea68909c2a66efd5ddb77fbaedd9a32
sunba <nsg2318@naver.com>
2021-09-22 20:17:58 +0900

    Model 파라미터화 V4

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,12 +1,12 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,17 +18,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 //                    v2/ 하위에 어떤 url이 들어와도 무조건 이 서블릿이 호출됨. 남)컨트롤러2들의 구현체들에 경로가 없었던 부분이 해결됨
-@WebServlet(name ="frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name ="frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
     //맵핑정보 어떤 url에 무슨 컨트롤러가 호출될 지. 그래서 인터페이스로 만들었나 ?
-    private Map<String, ControllerV3> controllerV3Map = new HashMap<>();
+    private Map<String, ControllerV4> controllerV4Map = new HashMap<>();
 
-    public FrontControllerServletV3() {
+    public FrontControllerServletV4() {
                                 // 이 url이 요청이 오면                        얘가 실행 됨
-        controllerV3Map.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerV3Map.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerV3Map.put("/front-controller/v3/members", new MemberListControllerV3());
+        controllerV4Map.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerV4Map.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerV4Map.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
@@ -36,25 +36,21 @@ public class FrontControllerServletV3 extends HttpServlet {
         //                    요청 들어온 url 반환
         String requestURI = request.getRequestURI();
         //                  각 url의 new로 생성된 "객체의 인스턴스"가 반환됨
-        ControllerV3 controllerV3 = controllerV3Map.get(requestURI);
+        ControllerV4 controllerV4 = controllerV4Map.get(requestURI);
 
         // url 이상한 거 처리
-        if(controllerV3 == null){
+        if(controllerV4 == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controllerV3.process(paramMap);
+        Map<String, Object> model = new HashMap<>(); // 추가
 
-        String viewName = mv.getViewName();//논리이름 new-form
+        String viewName = controllerV4.process(paramMap, model);
 
-        //뷰의 경로를 나는 그냥 viewName만 전달하고싶어서 메서드로 뽑았고 그 이름이 뷰 리졸버
         MyView view = viewResolver(viewName);
-
-        //Form에는 그냥 Model이 비어있을것임 그래서 render가 mv.getModel() 없이 될텐데 그럼 어케되는걸까
-        view.render(mv.getModel(),request,response);
-
+        view.render(model,request,response);
     }
 
     private MyView viewResolver(String viewName) {
